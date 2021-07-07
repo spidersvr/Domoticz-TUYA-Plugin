68430727e974d42a3e93f1a75e841768a6ded60c
Xenomes <xenomes@outlook.com>
2021-07-07 22:09:05 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -5,12 +5,12 @@
 # Contributed: Xenomes (xenomes@outlook.com)
 #
 """
-<plugin key="tuya" name="TUYA" author="Wagner Oliveira contributed Xenomes" version="1.0.10 alpha" wikilink="" externallink="https://github.com/Xenomes/Domoticz-TUYA-Plugin.git">
+<plugin key="tuya" name="TUYA" author="Wagner Oliveira contributed Xenomes" version="1.0.11 alpha" wikilink="" externallink="https://github.com/Xenomes/Domoticz-TUYA-Plugin.git">
     <description>
         Support forum: <a href="https://www.domoticz.com/forum/viewtopic.php?f=65&amp;t=33145">https://www.domoticz.com/forum/viewtopic.php?f=65&amp;t=33145</a><br/>
         Support forum Dutch: <a href="https://contactkring.nl/phpbb/viewtopic.php?f=60&amp;t=846">https://contactkring.nl/phpbb/viewtopic.php?f=60&amp;t=846</a><br/>
         <br/>
-        <h2>TUYA Plugin v.1.0.10 alpha</h2><br/>
+        <h2>TUYA Plugin v.1.0.11 alpha</h2><br/>
         This plugin is meant to control TUYA devices (mainly on/off switches and LED lights). TUYA devices may come with different brands and different Apps such as Smart Life or Jinvoo Smart, so select the corresponding App you're using below.
         <h3>Features</h3>
         <ul style="list-style-type:square">
@@ -277,6 +277,10 @@ class BasePlugin:
                     UpdateDevice(unit, 0, 'Off', not dev.available())
                 else:
                     UpdateDevice(unit, 1, 'On', not dev.available())
+                    
+                if dev.state() == True and not dev.available():
+                    UpdateDevice(unit, 0, 'Off', not dev.available())
+                    Domoticz.Log('DeviceID='+Devices[Unit].DeviceID+' Turned off because device is offline.')
 
         except Exception as err:
             Domoticz.Error("handleThread: "+str(err)+' line '+format(sys.exc_info()[-1].tb_lineno))
